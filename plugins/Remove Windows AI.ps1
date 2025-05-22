0a447af78f34f894616e444f3dc2a89d82ddd70b
Gurucharan S <70633240+GurucharanSavanth@users.noreply.github.com>
2025-05-22 11:54:43 +0530

    Update Remove Windows AI.ps1
    Updated RemoveWindowsAI.ps1 link

--- plugins/Remove Windows AI.ps1
+++ plugins/Remove Windows AI.ps1
@@ -1,1 +1,1 @@
-﻿iwr https://raw.githubusercontent.com/zoicware/RemoveWindowsAI/main/RemoveAi.ps1 | iex
+iwr https://raw.githubusercontent.com/zoicware/RemoveWindowsAI/main/RemoveWindowsAi.ps1 | iex
